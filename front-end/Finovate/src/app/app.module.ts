cbf13e40a3eb417ccf5cf62f048caacc799b6bd4
priyankachinchwade <priyanka.mokashi2022@gmail.com>
2023-03-02 18:29:17 +0530

    Add login UI

--- front-end/Finovate/src/app/app.module.ts
+++ front-end/Finovate/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http'
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,8 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     HttpClientModule,
     MatCardModule,
     MatToolbarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
