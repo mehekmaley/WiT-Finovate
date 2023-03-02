3ff5a3bc3a09df4241fb1cd5b3a1298628ba121b
unknown <komalkhedkar61@yahoo.com>
2023-03-02 12:35:28 +0530

    other changes

--- Komal-Python-FlaskEnv/uploadFile.py
+++ Komal-Python-FlaskEnv/uploadFile.py
@@ -1,9 +1,9 @@
 from azure.storage.blob import BlobServiceClient
 
-storage_account_key = "lg+SkFUDBH/r4LBKEY4Qs83QwkeAinrrpMoOhT9CTWLXzdAJ5Z6C/kncBr6i2fQ6p4EsqhDQOkNM+AStHTAcrg=="
-storage_account_name = "testkk"
-connection_string = "DefaultEndpointsProtocol=https;AccountName=testkk;AccountKey=zRajHglS/ubEApK7QMysa+kfr43ossjbiG043fVvZywR4j1QNl9dS6ueBCN+9cuI7ZifRQwJejaB+AStCAjSEw==;EndpointSuffix=core.windows.net"
-container_name = "fileholder"
+storage_account_key = "---lg+SkFUDBH/r4LBKEY4Qs83QwkeAinrrpMoOhT9CTWLXzdAJ5Z6C/kncBr6i2fQ6p4EsqhDQOkNM+AStHTAcrg==+++"
+storage_account_name = "---testkk+++"
+connection_string = "---DefaultEndpointsProtocol=https;AccountName=testkk;AccountKey=zRajHglS/ubEApK7QMysa+kfr43ossjbiG043fVvZywR4j1QNl9dS6ueBCN+9cuI7ZifRQwJejaB+AStCAjSEw==;EndpointSuffix=core.windows.net+++"
+container_name = "---fileholder+++"
 
 def uploadToBlobStorage(file_path, file_name):
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
